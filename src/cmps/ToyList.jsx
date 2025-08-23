5a75bcdbf13f82c0330f2ad411da1d3dd10016f2
EdenIvgi <edenavgi@gmail.com>
2025-08-23 13:00:40 +0300

    add toy list

---
+++ src/cmps/ToyList.jsx
@@ -0,0 +1,15 @@
+import { ToyPreview } from "./ToyPreview.jsx"
+
+export function ToyList({ toys, onRemoveToy }) {
+  if (!toys.length) return <p>No toys to show...</p>
+  return (
+    <ul className="toy-list clean-list">
+      {toys.map(toy =>
+        <li key={toy._id}>
+          <ToyPreview toy={toy} />
+          <button onClick={() => onRemoveToy(toy._id)}>Remove</button>
+        </li>
+      )}
+    </ul>
+  )
+}
